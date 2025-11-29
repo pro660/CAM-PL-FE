31992b33f754e546f1533b22fcdacf4c1e5cf5aa
pro660 <gw904202@gmail.com>
2025-11-30 03:26:09 +0900

    MapPage logic edit

--- src/components/map/NearbyPlaces.jsx
+++ src/components/map/NearbyPlaces.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import "../../css/map/NearbyPlaces.css";
 
-import PlusPlaceIcon from "../../images/map/pulsplace-icon.svg"
+import PlusPlaceIcon from "../../images/map/pulsplace-icon.svg";
 
 export default function NearbyPlaces({ places = [], onClickAdd }) {
   return (
@@ -25,13 +25,29 @@ export default function NearbyPlaces({ places = [], onClickAdd }) {
                     className="map-nearby-thumb"
                   />
                 </div>
+
                 <div className="map-nearby-info">
                   <div className="map-nearby-name">{place.name}</div>
                   <div className="map-nearby-category">
                     {place.category}
                   </div>
                 </div>
-                  <img src={PlusPlaceIcon} alt="장소 추가 아이콘"/>
+
+                {/* ✅ 플러스 아이콘 버튼으로 변경 + 클릭 시 onClickAdd(place) 호출 */}
+                <button
+                  type="button"
+                  className="map-nearby-add-btn"
+                  onClick={(e) => {
+                    e.stopPropagation(); // 혹시 li에 클릭 걸려 있으면 분리용
+                    onClickAdd?.(place);
+                  }}
+                  aria-label={`${place.name} 일정에 추가`}
+                >
+                  <img
+                    src={PlusPlaceIcon}
+                    alt="장소 추가 아이콘"
+                  />
+                </button>
               </li>
             ))}
           </ul>
