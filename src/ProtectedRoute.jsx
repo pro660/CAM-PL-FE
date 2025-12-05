142dd61e3cc8f5fd0df1bc2f8a6ea0f5034d11bd
pro660 <gw904202@gmail.com>
2025-12-06 01:54:27 +0900

    로그인 오류 수정

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,14 +1,14 @@
 // src/routes/ProtectedRoute.jsx
 import React from "react";
 import { Navigate, useLocation } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";        // <- 경로도 ../ 로
-import LoadingSpinner from "../components/common/Loader";
+import { useAuth } from "./context/AuthContext";
+import LoadingSpinner from "./components/common/Loader";
 
 export default function ProtectedRoute({ children }) {
-  const { isAuthenticated, loading } = useAuth();       // <- loading 으로 받기
+  const { isAuthenticated, isLoading } = useAuth();
   const location = useLocation();
 
-  if (loading) {
+  if (isLoading) {
     return <LoadingSpinner fullscreen />;
   }
 
@@ -18,3 +18,4 @@ export default function ProtectedRoute({ children }) {
 
   return children;
 }
+
