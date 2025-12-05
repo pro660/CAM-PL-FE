d0c7b98e839ecbee7e0d0e8229bbe51400ad785c
pro660 <gw904202@gmail.com>
2025-12-05 23:22:57 +0900

    edit css

--- src/components/mypage/Link_Box.jsx
+++ src/components/mypage/Link_Box.jsx
@@ -20,6 +20,7 @@ export default function LinkBox({
       onClick={onClick}
     >
       {/* 여기 텍스트 밑줄에 애니메이션 들어감 */}
+      <span className="mypage-link-box-label">{label}</span>
 
       <div className="mypage-link-box-icon">
         {iconSrc && (
@@ -30,7 +31,6 @@ export default function LinkBox({
           />
         )}
       </div>
-      <span className="mypage-link-box-label">{label}</span>
     </button>
   );
 }
